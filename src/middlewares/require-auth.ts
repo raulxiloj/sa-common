b90d97814d9fa72777f5779274680458b9600ae9
Raul Xiloj <raulxiloj98@gmail.com>
2021-08-31 04:45:49 -0600

    Updates

--- src/middlewares/require-auth.ts
+++ src/middlewares/require-auth.ts
@@ -1,7 +1,19 @@
 import { NextFunction, Request, Response } from "express";
+import jwt from 'jsonwebtoken';
 import { NotAuthorizedError } from "../errors/not-authorized-error";
+import { UserPayload } from "../interfaces/UserPayload";
 
 export const requireAuth = (req: Request, res: Response, next: NextFunction) => {
+
+    const token = req.headers.token as string;
+
+    if(!token)
+        return next();
+
+    try {
+        const payload = jwt.verify(token, 'secretKeyxd') as UserPayload;
+        req.currentUser = payload;
+    } catch (error) {}
 
     if(!req.currentUser)
         throw new NotAuthorizedError();
