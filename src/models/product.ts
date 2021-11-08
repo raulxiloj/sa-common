cd72d91bab79f16eb80fd02ff6e04a4b1a6d6d07
Raul Xiloj <raulxiloj98@gmail.com>
2021-11-08 02:16:35 -0600

    Updates

--- src/models/product.ts
+++ src/models/product.ts
@@ -18,7 +18,7 @@ interface ProductDoc extends mongoose.Document {
     foto: string;
     precio: number;
     stock: number;
-    categorias: string [];
+    categorias: Array<string>;
 }
 
 //Interface  for the properties that a product model has
